0fc1390e05690d5046d0242f4466519e68a6a03b
Batman <bgmyhan@gmail.com>
2020-04-25 21:32:38 -0700

    A working prototype

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-alert("Is This Thing on?");
 
 // Quiz questions 
 var quizQuestions = [
     {
-        question: "What? \(a) \(b) \(c) \(d) all of the above"
-        answer: "Yup"
+        question: "Where does the Javascript go? \r A <javascript> \r B) <span> \r C) <SCRIPT> \r D) <script>",
+        answer: "D"
     }, 
     {
-        question: "What? \(a) \(b) \(c) \(d) all of the above"
-        answer: "Yup"
+        question: "Is this the easiest quiz you've ever taken? \r A) Yes \r B) no \r C)Maybe \r D) Do I plan on improving this? Yes I understand my grade is reflected at the time of grading.",
+        answer: "A"
     }, 
     {
-        question: "What? \(a) \(b) \(c) \(d) all of the above"
-        answer: "Yup"
+        question: "Did Brandon try real hard at progaming me? \r A) Yes \r B) No \r C) Maybe \r D) If you tried I feel bad",
+        answer: "A"
     }, 
 ];
 
@@ -21,17 +20,17 @@ var quizQuestions = [
 var score = 0;
 
 for (var i = 0; i< quizQuestions.length; i++){
-    var userResponse = alert(quizQuestions[i].question)
-    if(userResponse = question[i].answer){
+    var userResponse = prompt(quizQuestions[i].question);
+    if(userResponse == quizQuestions[i].answer){
         score ++;
         alert("Correct!");
     }
         else{
-            alert("wrong...");
+            alert("That's just like, your opinion man!");
         }
-
-        alert (score + "/" + quizQuestions.Questions.length);
+       
+     
     }
-    
 
-console.log(quizQuestions)
+
+    alert ("Your Score is" + ' ' + score + "/" + quizQuestions.length);
